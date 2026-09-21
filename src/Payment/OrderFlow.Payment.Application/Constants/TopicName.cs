ae5a999eca070042fc793dc5c40d97cd5255a769
Nguyễn Minh Nhựt <0x6e687574@gmail.com>
2026-09-21 21:13:23 +0700

    fix(payment): wrong topic name

--- src/Payment/OrderFlow.Payment.Application/Constants/TopicName.cs
+++ src/Payment/OrderFlow.Payment.Application/Constants/TopicName.cs
@@ -5,5 +5,5 @@ public static class TopicName
     public const string DeadLetter = "dlq";
     public const string ReservationSucceeded = "reservation-succeeded";
     public const string PaymentSucceeded = "payment-succeeded";
-    public const string PaymentFailed = "payment-succeeded";
+    public const string PaymentFailed = "payment-failed";
 }
