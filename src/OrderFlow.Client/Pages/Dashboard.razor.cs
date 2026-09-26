52696f3bbb3b6d10fe25d5f22d7b0060a3c29300
Nguyễn Minh Nhựt <0x6e687574@gmail.com>
2026-09-26 14:23:27 +0700

    feat(client): add telerik

---
+++ src/OrderFlow.Client/Pages/Dashboard.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OrderFlow.Client.Pages;
+
+public partial class Dashboard : ComponentBase
+{
+}
---
+++ src/OrderFlow.Client/Pages/PlaceOrder.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OrderFlow.Client.Pages;
+
+public partial class PlaceOrder : ComponentBase
+{
+}
